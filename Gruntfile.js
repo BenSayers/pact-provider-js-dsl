7cf2ae4f7da6f043b60ef70a098f9b9a97032a10
Ben Sayers <bsayers@atlassian.com>
2015-03-05 11:02:36 +1100

    chore: Add changelog build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,21 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+        changelog: {
+            options: {
+                dest: 'docs/CHANGELOG.md'
+            }
+        },
         jasmine_node: {
             e2e: ['test/e2e'],
             unit: ['test/unit']
         }
     });
 
+    grunt.loadNpmTasks('grunt-conventional-changelog');
     grunt.loadNpmTasks('grunt-jasmine-node');
 
+
     grunt.registerTask('test', ['jasmine_node:unit', 'jasmine_node:e2e']);
+    grunt.registerTask('docs', ['changelog']);
     grunt.registerTask('default', []);
 };
