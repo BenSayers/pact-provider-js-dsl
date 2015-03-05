d8ac203ff873eb0e474379eac5ed4865918fcf4a
Ben Sayers <bsayers@atlassian.com>
2015-03-05 11:09:33 +1100

    chore: Add api doc build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,14 +8,20 @@ module.exports = function (grunt) {
         jasmine_node: {
             e2e: ['test/e2e'],
             unit: ['test/unit']
+        },
+        jsdoc2md: {
+            api: {
+                src: "lib/*.js",
+                dest: "docs/API.md"
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-conventional-changelog');
     grunt.loadNpmTasks('grunt-jasmine-node');
-
+    grunt.loadNpmTasks('grunt-jsdoc-to-markdown');
 
     grunt.registerTask('test', ['jasmine_node:unit', 'jasmine_node:e2e']);
-    grunt.registerTask('docs', ['changelog']);
+    grunt.registerTask('docs', ['changelog', 'jsdoc2md']);
     grunt.registerTask('default', []);
 };
